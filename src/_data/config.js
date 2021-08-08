03926ca01f1eaf7982ae12f144ddd2c6f644600d
Tim Carry <tim@pixelastic.com>
2021-08-08 14:31:22 +0200

    Add norska

---
+++ src/_data/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  algolia: {
+    appId: 'O3F8QXYK6R',
+    apiKey: 'b6a584fb2b5428dd72915e4b6128da0c',
+    indexName: 'gamemaster_terrainbuilding',
+  },
+};
---
+++ src/script.js
@@ -0,0 +1,22 @@
+const theme = require('norska/theme');
+const lazyloadHelper = require('norska/frontend/algolia/helpers/lazyload');
+
+(async () => {
+  await theme.init({
+    placeholder: 'Search for dungeon tiles, house, castle, sewers, anything!',
+    hitName: 'picture',
+    transforms: {
+      img(item) {
+        const originUrl = item.picture.url;
+        const options = {
+          imoen: item.picture,
+          uuid: item.objectID,
+          cacheBusting: false,
+        };
+        const img = lazyloadHelper.attributes(originUrl, options);
+
+        return img;
+      },
+    },
+  });
+})();
